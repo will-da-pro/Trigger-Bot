7d566bcb127f84cb160d18e28f1d8af2cad4a80b
will-da-pro <56065795+will-da-pro@users.noreply.github.com>
2022-11-29 07:05:14 +1100

    Update tronk.h

--- Clicker thing/tronk.h
+++ Clicker thing/tronk.h
@@ -6,10 +6,10 @@ using namespace std;
 
 class TriggerBot {
 public:
+	TriggerBot();
 	void activateBot();
 private:
 	bool checkKeyDown(int key);
 	INPUT initInput();
 	void click(INPUT ip);
-	COLORREF getPixelValue(HDC display);
 };
